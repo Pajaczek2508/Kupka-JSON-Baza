98ddedea72b99f9f09b100c32f5b8cb07f8496b5
Pajaczek2508 <pajaczek2508@gmail.com>
2026-09-24 08:04:19 +0200

    Add initial database of Python quiz questions

---
+++ db.js
@@ -0,0 +1,44 @@
+{
+  "pytania": [
+    {
+      "tresc": "Jaka jest najnowsza stabilna wersja python?",
+      "odpowiedzi": {
+        "A": "3.12",
+        "B": "3.13",
+        "C": "3.14",
+        "D": "3.15"
+      },
+      "poprawna": "C"
+    },
+    {
+      "tresc": "Jaka jest pierwsza wersja python?",
+      "odpowiedzi": {
+        "A": "0.9.0",
+        "B": "1.0.1",
+        "C": "2.0.0",
+        "D": "3.1.1"
+      },
+      "poprawna": "A"
+    },
+    {
+      "tresc": "Ile lat ma python?",
+      "odpowiedzi": {
+        "A": "20",
+        "B": "25",
+        "C": "30",
+        "D": "35"
+      },
+      "poprawna": "D"
+    },
+    {
+      "tresc": "Jak definiujemy funkcję w python?",
+      "odpowiedzi": {
+        "A": "function",
+        "B": "def",
+        "C": "func",
+        "D": "definition"
+      },
+      "poprawna": "B"
+    }
+  ]
+}
